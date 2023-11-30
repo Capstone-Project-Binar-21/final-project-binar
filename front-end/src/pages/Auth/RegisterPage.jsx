35ae42c17eb6f7c17f8211ebc1c33ad803add6ae
aliviakr <aliviak.r@gmail.com>
2023-11-30 20:02:13 +0700

    mengubah fungsi form menggunakan formik

--- front-end/src/pages/Auth/RegisterPage.jsx
+++ front-end/src/pages/Auth/RegisterPage.jsx
@@ -1,38 +1,45 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { toastNotify } from "../../libs/utils";
 
 const RegisterPage = () => {
-  const [form, setForm] = useState({
-    nama: "",
-    email: "",
-    nomorTelepon: "",
-    password: "",
-    konfirmasiPassword: "",
-  });
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (form.password !== form.konfirmasiPassword) {
-      return toastNotify({
-        type: "error",
-        message: "Password tidak sama",
+  const formik = useFormik({
+    initialValues: {
+      nama: "",
+      email: "",
+      nomorTelepon: "",
+      password: "",
+      konfirmasiPassword: "",
+    },
+    validationSchema: Yup.object({
+      nama: Yup.string().required("Nama harus diisi").min(3, "Minimal 3 huruf"),
+      email: Yup.string()
+        .email("Email tidak valid")
+        .required("Email harus diisi"),
+      nomorTelepon: Yup.string()
+        .required("Nomor Telepon harus diisi")
+        .min(9, "Nomor Telepon tidak valid"),
+      password: Yup.string()
+        .required("Password harus diisi")
+        .min(8, "Password minimal 8 karakter"),
+      konfirmasiPassword: Yup.string()
+        .required("Konfirmasi Password harus diisi")
+        .oneOf([Yup.ref("password"), null], "Password tidak sama"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      toastNotify({
+        type: "success",
+        message: "Berhasil mendaftar",
       });
-    }
-
-    console.log(form);
-    toastNotify({
-      type: "success",
-      message: "Berhasil mendaftar",
-    });
-  };
+      navigate("/auth/register/otp");
+    },
+  });
 
   return (
     <section className="h-[100vh] w-full grid lg:grid-cols-2 grid-cols-1 ">
@@ -42,85 +49,167 @@ const RegisterPage = () => {
         </h1>
         <form
           className="w-full flex flex-col gap-4 my-6"
-          onSubmit={handleSubmit}
+          onSubmit={formik.handleSubmit}
         >
-          <label className="form-control w-full">
+          <label className="form-control w-full relative">
             <div className="label">
-              <span className="lg:text-lg text-base font-medium">Nama</span>
+              <span
+                className={`lg:text-lg text-base font-medium ${
+                  formik.errors.nama && formik.touched.nama && "text-red-500"
+                }`}
+              >
+                Nama
+              </span>
             </div>
             <input
               type="text"
               placeholder="Nama Lengkap"
-              className="input input-bordered w-full"
+              className={`input input-bordered w-full ${
+                formik.errors.nama && formik.touched.nama && "input-error"
+              }`}
               name="nama"
-              value={form.nama}
-              onChange={handleChange}
-              required
+              value={formik.values.nama}
+              onChange={formik.handleChange}
             />
+            {formik.errors.nama && formik.touched.nama ? (
+              <div className="text-red-500">{formik.errors.nama}</div>
+            ) : formik.values.nama && !formik.errors.nama ? (
+              <div className="text-green-500 absolute bottom-3 right-3">
+                <CheckCircleIcon className="h-6 w-6" />
+              </div>
+            ) : null}
           </label>
 
-          <label className="form-control w-full">
+          <label className="form-control w-full relative">
             <div className="label">
-              <span className="lg:text-lg text-base font-medium">Email</span>
+              <span
+                className={`lg:text-lg text-base font-medium ${
+                  formik.errors.email && formik.touched.email && "text-red-500"
+                }`}
+              >
+                Email
+              </span>
             </div>
             <input
               type="email"
               placeholder="Contoh: johndee@gmail.com"
-              className="input input-bordered w-full"
+              className={`input input-bordered w-full ${
+                formik.errors.email && formik.touched.email && "input-error"
+              }`}
               name="email"
-              value={form.email}
-              onChange={handleChange}
-              required
+              value={formik.values.email}
+              onChange={formik.handleChange}
             />
+            {formik.errors.email && formik.touched.email ? (
+              <div className="text-red-500">{formik.errors.email}</div>
+            ) : formik.values.email && !formik.errors.email ? (
+              <div className="text-green-500 absolute bottom-3 right-3">
+                <CheckCircleIcon className="h-6 w-6" />
+              </div>
+            ) : null}
           </label>
 
-          <label className="form-control w-full">
+          <label className="form-control w-full relative">
             <div className="label">
-              <span className="lg:text-lg text-base font-medium">
+              <span
+                className={`lg:text-lg text-base font-medium ${
+                  formik.errors.nomorTelepon &&
+                  formik.touched.nomorTelepon &&
+                  "text-red-500"
+                }`}
+              >
                 Nomor Telepon
               </span>
             </div>
             <input
               type="text"
               placeholder="Contoh: +62.."
-              className="input input-bordered w-full"
+              className={`input input-bordered w-full ${
+                formik.errors.nomorTelepon &&
+                formik.touched.nomorTelepon &&
+                "input-error"
+              }`}
               name="nomorTelepon"
-              value={form.nomorTelepon}
-              onChange={handleChange}
-              required
+              value={formik.values.nomorTelepon}
+              onChange={formik.handleChange}
             />
+            {formik.errors.nomorTelepon && formik.touched.nomorTelepon ? (
+              <div className="text-red-500">{formik.errors.nomorTelepon}</div>
+            ) : formik.values.nomorTelepon && !formik.errors.nomorTelepon ? (
+              <div className="text-green-500 absolute bottom-3 right-3">
+                <CheckCircleIcon className="h-6 w-6" />
+              </div>
+            ) : null}
           </label>
 
-          <label className="form-control w-full">
+          <label className="form-control w-full relative">
             <div className="label">
-              <span className="lg:text-lg text-base font-medium">Password</span>
+              <span
+                className={`lg:text-lg text-base font-medium ${
+                  formik.errors.password &&
+                  formik.touched.password &&
+                  "text-red-500"
+                }`}
+              >
+                Password
+              </span>
             </div>
             <input
               type="password"
               placeholder="Masukkan Password"
-              className="input input-bordered w-full"
+              className={`input input-bordered w-full ${
+                formik.errors.password &&
+                formik.touched.password &&
+                "input-error"
+              }`}
               name="password"
-              value={form.password}
-              onChange={handleChange}
-              required
+              value={formik.values.password}
+              onChange={formik.handleChange}
             />
+            {formik.errors.password && formik.touched.password ? (
+              <div className="text-red-500">{formik.errors.password}</div>
+            ) : formik.values.password && !formik.errors.password ? (
+              <div className="text-green-500 absolute bottom-3 right-3">
+                <CheckCircleIcon className="h-6 w-6" />
+              </div>
+            ) : null}
           </label>
 
-          <label className="form-control w-full">
+          <label className="form-control w-full relative">
             <div className="label">
-              <span className="lg:text-lg text-base font-medium">
+              <span
+                className={`lg:text-lg text-base font-medium ${
+                  formik.errors.konfirmasiPassword &&
+                  formik.touched.konfirmasiPassword &&
+                  "text-red-500"
+                }`}
+              >
                 Konfirmasi Password
               </span>
             </div>
             <input
               type="password"
               placeholder="Konfirmasi Password"
-              className="input input-bordered w-full"
+              className={`input input-bordered w-full ${
+                formik.errors.konfirmasiPassword &&
+                formik.touched.konfirmasiPassword &&
+                "input-error"
+              }`}
               name="konfirmasiPassword"
-              value={form.konfirmasiPassword}
-              onChange={handleChange}
-              required
+              value={formik.values.konfirmasiPassword}
+              onChange={formik.handleChange}
             />
+            {formik.errors.konfirmasiPassword &&
+            formik.touched.konfirmasiPassword ? (
+              <div className="text-red-500">
+                {formik.errors.konfirmasiPassword}
+              </div>
+            ) : formik.values.konfirmasiPassword &&
+              !formik.errors.konfirmasiPassword ? (
+              <div className="text-green-500 absolute bottom-3 right-3">
+                <CheckCircleIcon className="h-6 w-6" />
+              </div>
+            ) : null}
           </label>
 
           <button type="submit" className="btn btn-primary self-center w-full">
