ec4faf34da0a14d43235e786a0c07b8541f2e2b7
raflimardhian <raflimardhian13@gamil.com>
2023-12-04 12:57:34 +0700

    fix error

--- back-end/models/index.js
+++ back-end/models/index.js
@@ -3,8 +3,8 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 module.exports = {
-    user : prisma.user,
-    profile : prisma.profile,
+    user : prisma.users,
+    profiles : prisma.Profile,
     category : prisma.course,
     course : prisma.course,
     order: prisma.order,
